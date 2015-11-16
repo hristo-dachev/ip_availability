001a89ef8ca1274af7e11511da1b2a740fffdd42
Hristo <hristo.dachev97@gmail.com>
2015-11-17 00:56:05 +0200

    Add methods to ComandHandler

--- src/ip_availability/CommandHandler.java
+++ src/ip_availability/CommandHandler.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 
 
 
+
 public class CommandHandler {
 	private Socket socket;
 	
@@ -11,8 +12,27 @@ public class CommandHandler {
 		this.socket = socket;
 	}
 	
+	public void start(){
+		//switch
+	}
+		
 	public String[] parse(String input) {
 		return input.split(":");
 	}
 	
+	public String login(){
+		
+	}
+	
+	public String logout(){
+		
+	}
+	
+	public String info(){
+		
+	}
+	
+	public String listavaliable(){
+		
+	}
 }
