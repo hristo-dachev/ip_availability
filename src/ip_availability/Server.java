03329e87ce167d53b1fb2f0112ad39a6f3d42c20
Hristo <hristo.dachev97@gmail.com>
2015-11-16 23:30:32 +0200

    Add server functionality

--- src/ip_availability/Server.java
+++ src/ip_availability/Server.java
@@ -1,5 +1,55 @@
 package ip_availability;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+
 public class Server {
+	private final Integer port;
+	private ServerSocket serverSocket;
+	private boolean running;
+	private final List<Client> clients = Collections.synchronizedList(new LinkedList<Client>());
 	
+	public Server(Integer port) {
+		this.port = port;
+	}
+	
+	public void startServer() throws IOException {
+		setRunning();
+		
+		this.serverSocket = new ServerSocket(port);
+		
+		while(isRunning())
+			//create client thread
+		
+		serverSocket.close();
+	}
+	
+	public synchronized void stopServer() throws IOException {
+		if (!running) throw new IllegalStateException("Not running");
+
+		running = false;
+		
+		serverSocket.close();
+		serverSocket = null;
+		
+		for (Client next : clients){}
+			//stop client
+	}
+	
+	public synchronized boolean isRunning() {
+		return running;
+	}
+	
+	private synchronized void setRunning() {
+		if(running) throw new IllegalStateException("Already running");
+		running = true;
+	}
+	
+	public synchronized void onClientStopped(Client clientHandler) {
+		clients.remove(clientHandler);
+	}
 }
--- src/ip_availability/StartServer.java
+++ src/ip_availability/StartServer.java
@@ -1,5 +1,13 @@
 package ip_availability;
 
-public class StartServer {
+import java.io.IOException;
 
+
+public class StartServer {
+	private static final Integer SERVER_PORT = 65535;
+	
+	public static void main(String[] args) throws IOException {
+		final Server server = new Server(SERVER_PORT);
+		server.startServer();
+	}
 }
