c7bb693dd3d9391ffca64157ebf089b1110a7720
Hristo <hristo.dachev97@gmail.com>
2015-11-17 02:47:16 +0200

    Add commandhandler in client

--- src/ip_availability/Client.java
+++ src/ip_availability/Client.java
@@ -9,10 +9,12 @@ import java.util.Scanner;
 public class Client implements Runnable {
 	private Socket socket;
 	private Server Server;
+	private CommandHandler commandLine;
 	
 	public Client(Socket socket, Server Server) {
 		this.socket = socket;
 		this.Server = Server;
+		this.commandLine = new CommandHandler(socket);
 	}
 	
 	@Override
@@ -20,7 +22,10 @@ public class Client implements Runnable {
 		try {
 			final PrintStream out = new PrintStream(socket.getOutputStream());
 			final Scanner scanner = new Scanner(socket.getInputStream());
-						
+			
+			while(scanner.hasNextLine()){
+				out.println(commandLine.start(commandLine.parse(scanner.nextLine()), Server));
+			}
 			scanner.close();
 			out.close();
 			
